ce90431ad05ca931a98ae9c8fc80ca15cb4e9c5f
Eoan O'Dea <odeaeoan@gmail.com>
2020-01-05 19:02:53 +0000

    Implemented task_timer layout xml file and  started adding UI items

--- app/src/main/java/ie/wspace/timemanagementapp/TimerActivity.java
+++ app/src/main/java/ie/wspace/timemanagementapp/TimerActivity.java
@@ -1,5 +1,15 @@
 package ie.wspace.timemanagementapp;
 
-public class TimerActivity {
+import android.os.Bundle;
+import android.os.PersistableBundle;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TimerActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
 }
